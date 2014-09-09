ed41856012045ca7c1c8ab8d9ca185df3b1c393c
kevinweber <kevinweber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-09 21:16:00 +0000

    Added tag 2.0.1 and 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/lazy-load-for-videos/tags/2.0.2@985729 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/min/admin-ck.js
@@ -0,0 +1,1 @@
+!function(n,$,o){$(document).ready(function(){i()});var i=function(){$("#tabs").tabs(),c(),t()},t=function(){$(".toggle").on("click",function(n){$(n.target).siblings(".toggle-me").toggle()})},c=function(){$("#llv_picker_player_colour").farbtastic("#llv_picker_input_player_colour")}}(window.incom=window.incom||{},jQuery);
