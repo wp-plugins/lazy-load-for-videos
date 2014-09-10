abe1f277839b7b273491ad7f85ceda8edb27be93
kevinweber <kevinweber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-10 18:49:43 +0000

    Added tag v2.0.3
    git-svn-id: https://plugins.svn.wordpress.org/lazy-load-for-videos/tags/2.0.3@986715 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/inc/class-update-posts.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Admin
+ */
+class Lazyload_Update_Posts {
+
+	/**
+	 * Use WordPress' built in function to delete oembed caches
+	 * Unused by core since WP 4.0.0 (http://developer.wordpress.org/reference/classes/wp_embed/delete_oembed_caches/)
+	 *
+	 * @since 1.6.2
+	 */
+	function delete_oembed_caches() {
+		global $wp_embed;
+	    
+	    $arr_posts = get_posts( array( 'post_type' => 'post', 'posts_per_page' => -1 ) );	// -1 == no limit
+
+	    foreach ( $arr_posts as $post ):
+	    	$wp_embed->delete_oembed_caches( $post->ID );
+	    endforeach;
+	}
+
+	/**
+	 * Delete cache for a single post
+	 * @since 2.0.3
+	 */
+	function delete_oembed_cache( $post_id ) {
+		global $wp_embed;
+		$wp_embed->delete_oembed_caches( $post_id );
+	}
+
+}
